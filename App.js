c5b15bd96dec187f9cc22baf05fbb194ff06a44d
Lavinia Fechete <lavinia.fechete22@gmail.com>
2021-02-16 14:59:02 +0200

    small fix

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
-import { NavigationMenu } from '../matchScreen/navigation/NavigationMenu';
+import { NavigationMenu } from './navigation/NavigationMenu';
 import { LoginContextProvider } from './LoginContext';
-import reactotron from 'reactotron-react-native';
 
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
@@ -11,27 +10,28 @@ import { loginReducer } from './store/reducers/loginReducer';
 import { goldReducer } from './store/reducers/goldReducer';
 import { colorsReducer } from './store/reducers/colorsReducer'
 
+import reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
-  import("./ReactotronConfig")
+	import("./ReactotronConfig")
 }
 
 const rootReducer = combineReducers({
-  login: loginReducer,
-  gold: goldReducer,
-  color: colorsReducer
+	login: loginReducer,
+	gold: goldReducer,
+	color: colorsReducer
 })
 
 const store = createStore(rootReducer);
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <LoginContextProvider>
-          <NavigationMenu />
-        </LoginContextProvider>
-      </Provider>
-    )
-  }
+	render() {
+		return (
+			<Provider store={store}>
+				<LoginContextProvider>
+					<NavigationMenu />
+				</LoginContextProvider>
+			</Provider>
+		)
+	}
 }
--- navigation/NavigationMenu.js
+++ navigation/NavigationMenu.js
@@ -12,8 +12,6 @@ import { LoginScreen } from '../screens/LoginScreen';
 import { RegisterScreen } from '../screens/RegisterScreen';
 import { ForgotPasswordScreen } from '../screens/ForgotPasswordScreen';
 
-//import { LoginContextConsumer } from '../LoginContext'
-
 import { useSelector } from 'react-redux';
 
 
@@ -22,35 +20,35 @@ import reactotron from 'reactotron-react-native';
 
 export const NavigationMenu = () => {
 
-    const Stack = createStackNavigator();
-    const isUserLogin = useSelector(state => state.login.login)
+	const Stack = createStackNavigator();
+	const isUserLogin = useSelector(state => state.login.login)
 
-    reactotron.log(isUserLogin, 'isUserlogin')
-    return (
-        < NavigationContainer >
-            <Stack.Navigator
-                screenOptions={
-                    {
-                        headerShown: false
-                    }
-                }
-            >
-                {!isUserLogin ? (
-                    <>
-                        <Stack.Screen name="Login" component={LoginScreen} />
-                        <Stack.Screen name="Password" component={ForgotPasswordScreen} />
-                        <Stack.Screen name="Register" component={RegisterScreen} />
-                    </>
-                ) : (
-                        <>
-                            <Stack.Screen name="Match" component={MatchScreen} />
-                            <Stack.Screen name="Profile" component={ProfileScreen} />
-                            <Stack.Screen name="Friends" component={FriendsScreen} />
-                            <Stack.Screen name="Messages" component={MessagesScreen} />
-                            <Stack.Screen name="Notifications" component={NotificationsScreen} />
-                        </>
-                    )}
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+	reactotron.log(isUserLogin, 'isUserlogin')
+	return (
+		< NavigationContainer >
+			<Stack.Navigator
+				screenOptions={
+					{
+						headerShown: false
+					}
+				}
+			>
+				{!isUserLogin ? (
+					<>
+						<Stack.Screen name="Login" component={LoginScreen} />
+						<Stack.Screen name="Password" component={ForgotPasswordScreen} />
+						<Stack.Screen name="Register" component={RegisterScreen} />
+					</>
+				) : (
+						<>
+							<Stack.Screen name="Match" component={MatchScreen} />
+							<Stack.Screen name="Profile" component={ProfileScreen} />
+							<Stack.Screen name="Friends" component={FriendsScreen} />
+							<Stack.Screen name="Messages" component={MessagesScreen} />
+							<Stack.Screen name="Notifications" component={NotificationsScreen} />
+						</>
+					)}
+			</Stack.Navigator>
+		</NavigationContainer>
+	)
 };
